14a7c503bd1efd4af8e967f8ff1ac13af0856253
Marta SH <msastreharo@gmail.com>
2021-07-29 14:55:44 +0200

    Changed styles in navbar, added Inicio page

--- web-client/src/js/components/NavBar.jsx
+++ web-client/src/js/components/NavBar.jsx
@@ -5,6 +5,7 @@ import transparentLogo from "./assets/econotravel-transparent-logo.png";
 export const NavBar = () => {
     return <nav className="navBar">
         <Link to={"/"} className="navBar__link"> <img className={"logo-img"} src={transparentLogo} /> </Link>
+        <Link to={"/"} className="navBar__link"> INICIO </Link>
         <Link to={"/experiences"} className="navBar__link">EXPERIENCIAS</Link>
         <Link to={"/about"} className="navBar__link">QUIÉNES SOMOS</Link>
         <Link to={"/reserva"} className="navBar__link">RESERVA</Link>
